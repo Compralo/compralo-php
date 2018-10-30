045f9e396760b368c4c944495b26169db7cb1b85
Equipe Compralo <44591162+compralo-team@users.noreply.github.com>
2018-10-30 00:57:13 -0300

    Add method CHECK

--- lib/Compralo.php
+++ lib/Compralo.php
@@ -16,42 +16,80 @@ class Compralo {
 
     public function create($store_name, $value, $description, $postback_url, $back_url=null){
 
-        $data = json_encode([
-            'api_key' => $this->apiKey,
-            'value' => $value,
-            'store_name' => $store_name,
-            'postback_url' => $postback_url,
-            'description' => $description,
-            'back_url' => $back_url,
-        ]);
+      $data = json_encode([
+          'api_key' => $this->apiKey,
+          'value' => $value,
+          'store_name' => $store_name,
+          'postback_url' => $postback_url,
+          'description' => $description,
+          'back_url' => $back_url,
+      ]);
 
-        $curl = curl_init();
+      $curl = curl_init();
 
-        curl_setopt_array($curl, array(
-          CURLOPT_URL => "https://app.compralo.com.br/api/v1/seller/generateInvoice",
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => "",
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 30,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => "POST",
-          CURLOPT_POSTFIELDS => $data,
-          CURLOPT_HTTPHEADER => array(
-            "Content-Type: application/json"
-          ),
-        ));
+      curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://app.compralo.com.br/api/v1/seller/generateInvoice",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => $data,
+        CURLOPT_HTTPHEADER => array(
+          "Content-Type: application/json"
+        ),
+      ));
 
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
+      $response = curl_exec($curl);
+      $err = curl_error($curl);
 
-        curl_close($curl);
+      curl_close($curl);
 
-        if ($err) {
-          return "cURL Error #:" . $err;
-        } else {
-            $response_data = json_decode($response);
-            return $response_data;
-        }
+      if ($err) {
+        return "cURL Error #:" . $err;
+      } else {
+          $response_data = json_decode($response);
+          return $response_data;
+      }
+
+    }
+
+    public function check($checkout_token){
+
+      $curl = curl_init();
+
+      curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://app.compralo.com.br/api/v1/seller/checkStatus/".$checkout_token,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => array(
+          "Content-Type: application/json"
+        ),
+      ));
+
+      $response = curl_exec($curl);
+      $err = curl_error($curl);
+
+      curl_close($curl);
+
+      if ($err) {
+        return "cURL Error #:" . $err;
+      } else {
+          $status_data = json_decode($response);
+
+          if($status_data->status == 1){
+            return 'complete';
+          }elseif($status_data->status == 3){
+            return 'expired';
+          }
+
+          return 'pending';
+      }
 
     }
 
