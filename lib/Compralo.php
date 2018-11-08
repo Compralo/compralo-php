f88d48c9c3399c35e716161b6690a15b5f8b85dd
Compralo Team <44591162+compralo-team@users.noreply.github.com>
2018-11-07 22:25:07 -0200

    Add new method to Withdrawal

--- lib/Compralo.php
+++ lib/Compralo.php
@@ -92,5 +92,44 @@ class Compralo {
       }
 
     }
+    
+    public function withdrawal($coin, $value, $destination){
+
+      $data = json_encode([
+          'api_key' => $this->apiKey,
+          'coin' => $coin,
+          'value' => $value,
+          'destination' => $destination,
+      ]);
+
+      $curl = curl_init();
+
+      curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://app.compralo.com.br/api/v1/withdrawal/create",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => $data,
+        CURLOPT_HTTPHEADER => array(
+          "Content-Type: application/json"
+        ),
+      ));
+
+      $response = curl_exec($curl);
+      $err = curl_error($curl);
+
+      curl_close($curl);
+
+      if ($err) {
+        return "cURL Error #:" . $err;
+      } else {
+          $response_data = json_decode($response);
+          return $response_data;
+      }
+
+    }
 
 }
